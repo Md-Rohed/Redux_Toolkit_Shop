ec4168bc66e2d3dfe5d81eca6c30b9e04e027e4a
Md-Rohed <mdrohed85@gmail.com>
2023-02-07 21:00:41 +0600

    input section added

--- src/feature/Book.jsx
+++ src/feature/Book.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
 import { ordered, restocked } from "./bookSlice";
 
 const Book = () => {
+  const [Value, setValue] = useState(1);
   const noOfBook = useSelector((state) => state.book.noOfBooks);
   const dispatch = useDispatch();
   return (
     <div>
       <h1> Total Book - {noOfBook} </h1>
-      <button onClick={() => dispatch(ordered())}>Ordered</button>
-      <button onClick={() => dispatch(restocked(5))}> Restocked</button>
+      <input
+        type="number"
+        value={Value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(ordered(Value))}>Ordered</button>
+      <button onClick={() => dispatch(restocked(Value))}> Restocked</button>
     </div>
   );
 };
